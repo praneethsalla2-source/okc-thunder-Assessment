b174f9d0336c9dfffd06b1814b46def11e68fa69
praneethsalla2-source <praneethsalla2@gmail.com>
2025-10-06 18:18:11 -0500

    Add https:// prefix to backend domain URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BACKEND_PUBLIC_DOMAIN: 'backend-production-c87e.up.railway.app'
+  BACKEND_PUBLIC_DOMAIN: 'https://backend-production-c87e.up.railway.app'
 };
