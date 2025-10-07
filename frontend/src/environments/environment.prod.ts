f4c0ec4a12ea6b23047e78827def286ef48b82af
praneethsalla2-source <praneethsalla2@gmail.com>
2025-10-06 19:51:18 -0500

    Update production environment with new Railway backend domain

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BACKEND_PUBLIC_DOMAIN: 'https://backend-production-ddeb.up.railway.app'
+  BACKEND_PUBLIC_DOMAIN: 'https://backend-production-3221.up.railway.app'
 };
